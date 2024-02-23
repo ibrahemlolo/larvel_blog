216acfc8ef9e7c8c7f4890c0299e93eae9a6ca0b
ibrahemlolo <ibrahemlolo.sy@gmail.com>
2024-02-23 16:20:42 +0300

    First Laravel Project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+
+ <div class="hero-bg-image flex flex-col items-center justify-center">
+      <h1 class="text-gray-100 text-5xl uppercase font-bold pb-10">welcome to my Blog </h1>
+      <a href="/" class="bg-gray-100 text-gray-700 py-7 px-5 rounded-lg font-bold uppercase ">Start Reading</a>
+ </div>  
+
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,47 @@
+<footer class="bg-gray-700 py-15 mt-20">
+
+<div class="container mx-auto flex justify-between item-center px-6">
+      <div>
+
+         <h3>Pages</h3>
+         <ul class="pt-4 text-gray-400 ">
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/">home</a>
+          </li>
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/blog" >blog</a>
+          </li>
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/login" >login</a>
+          </li>
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/register" >Register</a>
+          </li>
+            
+         </ul>
+
+      </div>
+
+          <div>
+
+         <h3>Latest Posts</h3>
+         <ul class="pt-4 text-gray-400 ">
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/">home</a>
+          </li>
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/" >blog</a>
+          </li>
+          <li class="pb-2">
+           <a class="hover:text-gray-100 transition duration-300" href="/" >login</a>
+          </li>
+        
+            
+         </ul>
+
+      </div>
+       
+</div>
+
+
+</footer>
